f9b429d4a1defa5fc06b62789601f4c17e132de5
marshalltaylorSFE <marshall.taylor@sparkfun.com>
2016-02-19 18:31:48 -0700

    quad test

--- TwoSinesOutputTest/TwoSinesOutputTest.ino
+++ TwoSinesOutputTest/TwoSinesOutputTest.ino
@@ -1,50 +1,54 @@
 #include <Audio.h>
 #include <Wire.h>
 #include <SPI.h>
-//#include <SD.h>
-//#include <SerialFlash.h>
+#include <SD.h>
+#include <SerialFlash.h>
 
 // GUItool: begin automatically generated code
-AudioSynthWaveformSine   sine1;          //xy=383,308
-AudioSynthWaveformSine   sine2;          //xy=383,368
-AudioFilterStateVariable filter1;        //xy=619,308
-AudioFilterStateVariable filter2;        //xy=619,378
-AudioFilterStateVariable filter3;        //xy=819,308
-AudioFilterStateVariable filter4;        //xy=820,390
-AudioOutputI2S           i2s1;           //xy=996,345
-AudioConnection          patchCord1(sine1, 0, filter1, 0);
-AudioConnection          patchCord2(sine2, 0, filter2, 0);
-AudioConnection          patchCord3(filter1, 0, filter3, 0);
-AudioConnection          patchCord4(filter2, 0, filter4, 0);
-AudioConnection          patchCord5(filter3, 0, i2s1, 0);
-AudioConnection          patchCord6(filter4, 0, i2s1, 1);
-AudioControlSGTL5000     sgtl5000_1;     //xy=452,593
+AudioSynthWaveformSine   sine4;          //xy=262,521
+AudioSynthWaveformSine   sine1;          //xy=266,301
+AudioSynthWaveformSine   sine2;          //xy=266,361
+AudioSynthWaveformSine   sine3;          //xy=273,443
+AudioOutputI2SQuad       i2s_quad1;      //xy=882,354
+AudioConnection          patchCord1(sine4, 0, i2s_quad1, 3);
+AudioConnection          patchCord2(sine1, 0, i2s_quad1, 0);
+AudioConnection          patchCord3(sine2, 0, i2s_quad1, 1);
+AudioConnection          patchCord4(sine3, 0, i2s_quad1, 2);
+AudioControlSGTL5000     sgtl5000_1;     //xy=651,514
+AudioControlSGTL5000     sgtl5000_2;     //xy=675,568
 // GUItool: end automatically generated code
-
-
 
 
 void setup() {
   // put your setup code here, to run once:
-  AudioMemory(18);
+  AudioMemory(35);
   
+  sgtl5000_1.setAddress(LOW);
   sgtl5000_1.enable();
   sgtl5000_1.volume(0.5);
-  sgtl5000_1.unmuteHeadphone();
+
+  sgtl5000_2.setAddress(HIGH);
+  sgtl5000_2.enable();
+  sgtl5000_2.volume(0.5);
+
   sine1.amplitude(1);
-  sine1.frequency(199);
+  sine1.frequency(50);
   
   sine2.amplitude(1);
-  sine2.frequency(201);
+  sine2.frequency(80);
 
-  filter1.frequency(600);  
-  filter2.frequency(600);  
-  filter3.frequency(600);  
-  filter4.frequency(600);  
+  sine3.amplitude(1);
+  sine3.frequency(192);
+  
+  sine4.amplitude(1);
+  sine4.frequency(385);
+
+
 
 }
 
 void loop() {
+	while(1);
   // put your main code here, to run repeatedly:
   sine1.frequency(199);
   sine2.frequency(201);	
